93dbc595d4e5fea71a8d6e87db10d4496e1a6ca5
Dayeun Choi <120041848+ChoiDayeun@users.noreply.github.com>
2022-12-15 08:00:05 +0900

    Update st_soyeong_add.py

--- st_soyeong_add.py
+++ st_soyeong_add.py
@@ -15,7 +15,6 @@ from collections import Counter
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.tag import pos_tag
-/home/appuser/venv/bin/python -m pip install --upgrade pip
 
 nltk.download('omw-1.4')
 nltk.download('punkt')
